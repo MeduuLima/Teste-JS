99bb33ece9220e91c337eb2cd1b34ef7424519d0
Maria Lima <127319517+MeduuLima@users.noreply.github.com>
2024-02-27 10:17:58 -0300

    .

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ function mudarCorTexto(){
     document.querySelector("#p1").style.color= "pink"
    
 }                                                   
-                        
+                    
+function mudarCorTexto(){
+   
+    document.querySelector("#p1").style.color= "aqua"
+   
+}                                            
